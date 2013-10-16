bbb5da66ab6335a1726e990766079894db1b7ee7
kuehnerbnu <kuehner@bnu.de>
2013-10-16 16:57:12 +0200

    first commit
    tutorial example

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+Z:\wwwroot\app\tutorial4\platforms\android\gen\com\phonegap\hello_world\R.java \
+ : Z:\wwwroot\app\tutorial4\platforms\android\res\drawable-xhdpi\icon.png \
+Z:\wwwroot\app\tutorial4\platforms\android\res\values\strings.xml \
+Z:\wwwroot\app\tutorial4\platforms\android\res\drawable-ldpi\icon.png \
+Z:\wwwroot\app\tutorial4\platforms\android\res\drawable\icon.png \
+Z:\wwwroot\app\tutorial4\platforms\android\res\drawable-mdpi\icon.png \
+Z:\wwwroot\app\tutorial4\platforms\android\res\xml\config.xml \
+Z:\wwwroot\app\tutorial4\platforms\android\res\drawable-hdpi\icon.png \
+Z:\wwwroot\app\tutorial4\platforms\android\bin\AndroidManifest.xml \
